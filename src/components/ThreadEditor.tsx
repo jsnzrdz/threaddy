ecae6efc1314eab75958811eb2cc3aff330935fd
jsnzrdz <jsnzrdz@gmail.com>
2024-08-22 11:05:27 +0200

    Refactor

--- src/components/ThreadEditor.tsx
+++ src/components/ThreadEditor.tsx
@@ -2,43 +2,60 @@ import { useState } from "react"
 import { TweetModel } from "../models/tweet-model";
 import TweetEditor from "./TweetEditor";
 
-const fakeTweets: TweetModel[] = [
+const initialThread: TweetModel[] = [
     {
         threadPosition: 0,
         textContent: ""
-    }
+    } as TweetModel
 ]
 
-
-
 export default function ThreadEditor() {
-    const [tweets, setTweets] = useState<TweetModel[]>(fakeTweets);
+    const [tweets, setTweets] = useState<TweetModel[]>(initialThread);
 
-    function addTweet() {
+    // Simplificado para usar el estado anterior.
+    const addTweet = () => {
+        setTweets(prevTweets => [
+            ...prevTweets,
+            { threadPosition: prevTweets.length, textContent: "" }
+        ]);
+    };
 
-        // Crear el nuevo tweet con una posición siguiente
-        const newTweet = new TweetModel();
-        newTweet.threadPosition = tweets.length;
+    // Utilización de funciones inmutables para actualizar el estado.
+    const updateTweetContent = (index: number, value: string) => {
+        setTweets(prevTweets => prevTweets.map((tweet, i) =>
+            i === index ? { ...tweet, textContent: value } : tweet
+        ));
+    };
 
-        console.log("new teweet", newTweet);
-        setTweets([...tweets, newTweet])
-    }
+    // Eliminación de tweet y reindexación en una sola operación inmutable.
+    const deleteTweet = (index: number) => {
+        setTweets(prevTweets => {
+            const updatedTweets = prevTweets.filter((_, i) => i !== index);
+            return updatedTweets.map((tweet, i) => ({ ...tweet, threadPosition: i }));
+        });
+    };
 
     return (
-        <div className="min-w-0 w-[30rem] my-16 mx-8 md:mx-0 flex flex-col gap-4">
+        <div className="min-w-0 w-[30rem] my-16 mx-4 md:mx-0 flex flex-col gap-4">
             <h2 className="text-xl font-semibold text-center">¡Empieza a escribir tu hilo! 🧵</h2>
 
             <ol className="mt-6 flex flex-col gap-4">
                 {
                     tweets.map((tweet, index) =>
-                        <li key={index}>
-                            <TweetEditor tweet={tweet} />
+                        <li key={tweet.threadPosition}>
+
+                            <TweetEditor
+                                tweet={tweet}
+                                isTheOnlyTweet={tweets.length === 1}
+                                onUpdateContent={updateTweetContent}
+                                onDeleteTweet={deleteTweet}
+                            />
 
                             <div className="mt-4">
                                 {
                                     // Mostrar el botón de añadir si es el último tweet
                                     index === tweets.length - 1 &&
-                                    <div className="flex flex-col gap-4 md:flex-row md:justify-between">                                    
+                                    <div className="flex flex-col gap-4 md:flex-row md:justify-between">
                                         <button
                                             className="min-w-28 py-2 px-3 border border-white rounded-md font-semibold tracking-wider"
                                             onClick={addTweet}
--- src/components/TweetEditor.tsx
+++ src/components/TweetEditor.tsx
@@ -1,8 +1,11 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { TweetModel } from "../models/tweet-model";
 
 interface TweetEditorProps {
-    tweet: TweetModel
+    tweet: TweetModel,
+    isTheOnlyTweet: boolean,
+    onUpdateContent: (index: number, textContent: string) => void,
+    onDeleteTweet: (index: number) => void
 }
 
 // Máximo de caracteres de un tweet
@@ -13,27 +16,46 @@ function getAvailableChars(currentTextContent: string) {
     return (AVAILABLE_CHARS - currentTextContent.length).toString();
 }
 
-export default function TweetEditor({ tweet }: TweetEditorProps) {
+export default function TweetEditor({ tweet, isTheOnlyTweet, onUpdateContent, onDeleteTweet }: TweetEditorProps) {
 
     // Contenido de tipo texto del tweet
     const [currentTextContent, setCurrentTextContent] = useState<string>(tweet.textContent ? tweet.textContent : "");
 
+    // Callback de edición para actualizar estado interno del componente y el general del threadEditor
+    function updateTextContentState(value: string) {
+        setCurrentTextContent(value);
+        onUpdateContent(tweet.threadPosition, value);
+    }
+
+    // useEffect para sincronizar el estado local con el estado global cuando el tweet cambia
+    useEffect(() => {
+        setCurrentTextContent(tweet.textContent);
+    }, [tweet.textContent]);
+
     return (
-        <div className="flex-grow min-w-0 max-w-lg">
+        <div className="flex-grow min-w-0 max-w-lg animate-in fade-in duration-1000">
 
             <div className="relative">
                 <textarea
                     className="py-3 px-4 h-32 w-full bg-gray-900/40 text-white border border-white rounded-lg resize-none"
                     value={currentTextContent}
                     placeholder="Escribe tu tweet aquí..."
-                    onChange={(e) => setCurrentTextContent(e.target.value)}
+                    onChange={(e) => updateTextContentState(e.target.value)}
                 />
                 <div className="absolute right-2 bottom-2 text-sm text-gray-400">
                     {getAvailableChars(currentTextContent)}
                 </div>
-                <div className="absolute -right-6 -top-4 font-bold text-4xl text-red-600 cursor-pointer">
-                    -
-                </div>
+                {
+                    isTheOnlyTweet ?
+                        "" :
+                        <div
+                            className="absolute -right-6 -top-4 font-bold text-4xl text-red-600 cursor-pointer"
+                            onClick={() => onDeleteTweet(tweet.threadPosition)}
+                        >
+                            -
+                        </div>
+
+                }
             </div>
         </div>
     )
