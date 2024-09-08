6af8906865eb1962532aaa640b19224a77705599
Jaime Sánchez <jsanchez@surcontrol.com>
2024-09-08 19:34:22 +0200

    Integrando testing con vitest

---
+++ src/components/FakeComponent/FakeComponent.tsx
@@ -0,0 +1,5 @@
+export default function FakeComponent() {
+  return (
+    <div>fake Component</div>
+  )
+}
--- vite.config.ts
+++ vite.config.ts
@@ -1,3 +1,5 @@
+/// <reference types="vitest/config" />
+
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
@@ -5,6 +7,12 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server: {
-    
+  
+  },
+  test: {
+    globals: true,
+    environment: 'jsdom',
+    css: true,
+    setupFiles : './src/test/setup.ts'
   }
 })
