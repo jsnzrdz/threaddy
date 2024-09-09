ccd6362916342af14e063fee50efea3fbb78b190
Jaime Sánchez <jsanchez@surcontrol.com>
2024-09-09 19:41:20 +0200

    Usar aria-label para acceder a los elementos del DOM en los test de tweetActionButtons. Uso de beforeEach para crear el modelo y renderizarlo para cada caso de uso. Separación en casos de uso.

--- src/components/TweetEditor/TweetActionButtons/TweetActionButtons.test.tsx
+++ src/components/TweetEditor/TweetActionButtons/TweetActionButtons.test.tsx
@@ -6,98 +6,83 @@ import TweetActionButtons from "./TweetActionButtons";
 const mockOnMoveTweet = vitest.fn();
 const mockOnDeleteTweet = vitest.fn();
 
-describe("TweetActionButtons Component", () => {
+describe("TweetActionButtons component render", () => {
 
-    const tweet: TweetModel = {
-        threadPosition: 0,
-        textContent: "",
-        mediaContent: [null, null, null, null]
-    };
+    let tweet: TweetModel;
+    describe("Initial tweet in a empty thread", () => {
 
-    // Caso de uso inicial (hilo con 1 tweet vacío)
-    it("should not render arrow up button if tweet is the only tweet in the thread", () => {
-        render(
-            <TweetActionButtons 
-                threadLength={1}
-                tweet={{...tweet, threadPosition: 0}}
-                onMoveTweet={mockOnMoveTweet}
-                onDeleteTweet={mockOnDeleteTweet}
-            />
-        )
-
-        const arrowUpElement = screen.queryByTestId('arrow-up');
-        expect(arrowUpElement).toBeNull();
-    });
-
-    it("should not render arrow down button if tweet is the only tweet in the thread", () => {
-        render(
-            <TweetActionButtons 
-                threadLength={1}
-                tweet={tweet}
-                onMoveTweet={mockOnMoveTweet}
-                onDeleteTweet={mockOnDeleteTweet}
-            />
-        )
-
-        const arrowUpElement = screen.queryByTestId('arrow-down');
-        expect(arrowUpElement).toBeNull();
-    });
-
-    it("should not render delete button if tweet is the only tweet in the thread", () => {
-        render(
-            <TweetActionButtons 
-                threadLength={1}
-                tweet={tweet}
-                onMoveTweet={mockOnMoveTweet}
-                onDeleteTweet={mockOnDeleteTweet}
-            />
-        )
-
-        const arrowUpElement = screen.queryByTestId('delete');
-        expect(arrowUpElement).toBeNull();
-    });
-
-    // Primer tweet de un hilo que tiene más de 1 tweet
+        // Modelo de tweet inicial y una longitud de 1 solo tweet
+        beforeEach(() => {
+            tweet = {
+                threadPosition: 0,
+                textContent: "",
+                mediaContent: [null, null, null, null]
+            }
     
-    it("should render the arrow down button if it is the first tweet in a thread with more than one tweet", () => {
-        render(
-            <TweetActionButtons 
-                threadLength={2}
-                tweet={{...tweet, threadPosition: 0}}
-                onMoveTweet={mockOnMoveTweet}
-                onDeleteTweet={mockOnDeleteTweet}
-            />
-        )
+            render(
+                <TweetActionButtons 
+                    threadLength={1}
+                    tweet={tweet}
+                    onMoveTweet={mockOnMoveTweet}
+                    onDeleteTweet={mockOnDeleteTweet}
+                />
+            )
+        });
 
-        const arrowUpElement = screen.queryByTestId('arrow-down');
-        expect(arrowUpElement).toBeInTheDocument();
+        it("should not render arrow up button if tweet is the only tweet in the thread", () => {
+            const arrowUpElement = screen.queryByLabelText('Move tweet up');
+            expect(arrowUpElement).toBeNull();
+        });
+    
+        it("should not render arrow down button if tweet is the only tweet in the thread", () => {
+            const arrowUpElement = screen.queryByLabelText('Move tweet down');
+            expect(arrowUpElement).toBeNull();
+        });
+    
+        it("should not render delete button if tweet is the only tweet in the thread", () => {
+            const arrowUpElement = screen.queryByLabelText('Delete tweet');
+            expect(arrowUpElement).toBeNull();
+        });
     })
 
-    it("should not render the arrow up button if it is the first tweet in a thread with more than one tweet", () => {
-        render(
-            <TweetActionButtons 
-                threadLength={2}
-                tweet={{...tweet, threadPosition: 0}}
-                onMoveTweet={mockOnMoveTweet}
-                onDeleteTweet={mockOnDeleteTweet}
-            />
-        )
+    describe("First tweet in a thread with more than 1 tweet", () => {
 
-        const arrowDownElement = screen.queryByTestId('arrow-up');
-        expect(arrowDownElement).toBeNull();
+        // Primer tweet de un hilo que tiene más de 1 tweet
+        beforeEach(() => {
+            tweet = {
+                threadPosition: 0,
+                textContent: "",
+                mediaContent: [null, null, null, null]
+            }
+    
+            render(
+                <TweetActionButtons 
+                    threadLength={5}
+                    tweet={tweet}
+                    onMoveTweet={mockOnMoveTweet}
+                    onDeleteTweet={mockOnDeleteTweet}
+                />
+            );
+        });
+
+        it("should render the arrow down button if it is the first tweet in a thread with more than one tweet", () => {   
+            const arrowUpElement = screen.queryByLabelText('Move tweet down');
+            expect(arrowUpElement).toBeInTheDocument();
+        });
+    
+        it("should not render the arrow up button if it is the first tweet in a thread with more than one tweet", () => {   
+            const arrowDownElement = screen.queryByLabelText('Move tweet up');
+            expect(arrowDownElement).toBeNull();
+        });
+    
+        it("should render the delete button if it is the first tweet in a thread with more than one tweet", () => {   
+            const arrowDownElement = screen.queryByLabelText('Delete tweet');
+            expect(arrowDownElement).toBeInTheDocument();
+        });
+
     })
 
-    it("should render the delete button if it is the first tweet in a thread with more than one tweet", () => {
-        render(
-            <TweetActionButtons 
-                threadLength={2}
-                tweet={{...tweet, threadPosition: 0}}
-                onMoveTweet={mockOnMoveTweet}
-                onDeleteTweet={mockOnDeleteTweet}
-            />
-        )
-
-        const arrowDownElement = screen.queryByTestId('delete');
-        expect(arrowDownElement).toBeInTheDocument();
-    })
+    afterEach(() => {
+        vitest.clearAllMocks();
+    });
 });
